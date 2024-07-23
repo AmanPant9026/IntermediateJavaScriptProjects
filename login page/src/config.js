fd20ebc1062df64c25012375f291ffd32c765eb9
AMAN <pantaman98@gmail.com>
2024-07-23 14:05:47 +0530

    added login page

---
+++ login page/src/config.js
@@ -0,0 +1,27 @@
+const mongoose=require("mongoose");
+const connect=mongoose.connect("mongodb+srv://pantaman98:aman989768052@cluster0.h06v34q.mongodb.net/Netflix");
+
+//check database connected or not
+connect.then(()=>{
+console.log("Database connected successfully");
+})
+.catch(()=>{
+    console.log("Database cannot be connected");
+})
+
+//create a schema
+const LoginSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+});
+
+//collection Part
+const collection=new mongoose.model("users",LoginSchema);
+
+module.exports=collection;
